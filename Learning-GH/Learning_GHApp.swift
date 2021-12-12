edfa2d21c695952819ad972f6605f80879d456b1
Nicolás Táutiva <33504315+NickATC@users.noreply.github.com>
2021-12-11 20:02:34 -0500

    Initial Commit

---
+++ Learning-GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning-GH
+//
+//  Created by NICOLAS  TAUTIVA on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
